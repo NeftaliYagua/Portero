635f1a44d2d149ce06f49df1791a02d2c2e6d64b
Neftalí Yagua <despacho@neftaliyagua.com.ve>
2018-05-24 11:50:37 -0400

    Tema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body>
         <div id="app" class="container">
-            <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <nav class="navbar navbar-expand-md navbar-dark bg-primary navbar-laravel">
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
